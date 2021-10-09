bb99544d19187dd3aff2c6ab1352556a9607502e
Olena Stadnyk <lenesx@gmail.com>
2021-10-10 00:46:51 +0300

    feat: add gallery section

--- js/main.js
+++ js/main.js
@@ -6,5 +6,23 @@ $('popup__link').magnificPopup({
 		removalDelay: 160,
 		preloader: false,
 		fixedContentPos: false
+});
+	
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick_btn slick_prev"><img src="images/arrow-left.svg" alt="" /></button>',
+		nextArrow: '<button type="button" class="slick_btn slick_next"><img src="images/arrow-right.svg" alt="" /></button>',
+	});
+
+	$('.gallery__item_inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		
 	});
 });
